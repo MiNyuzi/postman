c2857a1472741d1f4edda8a2065facf3b7a5f388
shysia <ehgmlwlas@naver.com>
2023-11-09 18:13:23 +0900

    comment

---
+++ src/main/java/application/web/dto/CommDto.java
@@ -0,0 +1,31 @@
+package application.web.dto;
+
+import application.entity.CommEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommDto {
+    private Integer id;
+    private String author;
+    private String content;
+    private Integer post_id;
+    private LocalDateTime comm_at;
+
+    
+    //엔티티를 디티오로 변환
+    public static CommDto toCommDto(CommEntity comment, Integer post_id) {
+        CommDto commDto = new CommDto();
+        commDto.setId(comment.getId());
+        commDto.setAuthor(comment.getAuthor());
+        commDto.setContent(comment.getContent());
+//        commDto.setPost_id(comment.getPostEntity().getId());
+        commDto.setPost_id(post_id);
+        return commDto;
+    }
+}
