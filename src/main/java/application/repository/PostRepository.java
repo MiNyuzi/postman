d35ad46dbba7c8a10acc8053a22a28fa08cb2838
shysia <ehgmlwlas@naver.com>
2023-11-10 00:26:55 +0900

    Post Repository

---
+++ src/main/java/application/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package application.repository;
+
+import application.entity.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+    Page<PostEntity> findByTitleContaining(String searchKeyword, Pageable pageable);
+}
