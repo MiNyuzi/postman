a84a6e0d13135146d8edfd01acd290cbec882998
kminsu05 <kminsu05@gmail.com>
2023-11-09 17:53:42 +0900

    restart

---
+++ src/test/java/com/postman1/postman1/Postman1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postman1.postman1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Postman1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
