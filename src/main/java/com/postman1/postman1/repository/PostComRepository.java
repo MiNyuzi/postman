bf39f9597e0968ad10c61615523d89c5fbb1000c
kminsu05 <kminsu05@gmail.com>
2023-11-10 11:53:46 +0900

    last

--- src/main/java/com/postman1/postman1/repository/PostComRepository.java
+++ src/main/java/com/postman1/postman1/repository/PostComRepository.java
@@ -1,4 +1,4 @@
-package application.repository;
+package com.postman1.postman1.repository;
 
 import com.postman1.postman1.entity.PostEntity;
 import org.springframework.data.domain.Page;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+public interface PostComRepository extends JpaRepository<PostEntity, Integer> {
     Page<PostEntity> findByTitleContaining(String searchKeyword, Pageable pageable);
 
 }
--- src/main/java/com/postman1/postman1/service/CommService.java
+++ src/main/java/com/postman1/postman1/service/CommService.java
@@ -3,7 +3,7 @@ package com.postman1.postman1.service;
 import com.postman1.postman1.entity.CommEntity;
 import com.postman1.postman1.entity.PostEntity;
 import com.postman1.postman1.repository.CommRepository;
-import application.repository.PostRepository;
+import com.postman1.postman1.repository.PostComRepository;
 import com.postman1.postman1.web.dto.CommDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,12 +16,12 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CommService {
     private final CommRepository commRepository;
-    private final PostRepository postRepository;
+    private final PostComRepository postComRepository;
 
     // Dto를 Entity로 변환
     public Integer save(CommDto commDto) {
         //부모엔티티(PostEntity) 조회
-        Optional<PostEntity> optionalPost = postRepository.findById(commDto.getPost_id());
+        Optional<PostEntity> optionalPost = postComRepository.findById(commDto.getPost_id());
         //부모엔티티 있을 때 댓글 저장
         if (optionalPost.isPresent()) {
             PostEntity post = optionalPost.get();
@@ -35,7 +35,7 @@ public class CommService {
     public List<CommDto> findAll(Integer post_id) {
         //select * from comments_table where post_id=? order by id dasc;
         //게시글 기준으로 목록을 가져오고 아이디 기준으로 내림차순
-        PostEntity post = postRepository.findById(post_id).get();
+        PostEntity post = postComRepository.findById(post_id).get();
         List<CommEntity> commentList = commRepository.findAllByPostOOrderByIdDesc(post);
         //Entity 리스트를 Dto 리스트로 바꿔주기
         List<CommDto> commDtoList = new ArrayList<>();
